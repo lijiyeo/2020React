9b5b0981c0c629719ff3e42b222c28c277e89eb9
lijiyeo <ljy981017>
2020-10-06 14:06:40 +0800

    1

---
+++ build/precache-manifest.4b48e608c9508249975acca233a46983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc49e34e403ea2fa4668100294023a16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "411838e9fc5d177c8f1a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "095dac1b4a988e142d3a",
+    "url": "/static/js/2.aeb85de7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aeb85de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411838e9fc5d177c8f1a",
+    "url": "/static/js/main.20b2bad4.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
