cfdc9f4a3c2472001d2e89d1850816f5c35eab01
lijiyeo <39698638+lijiyeo@users.noreply.github.com>
2020-10-18 10:40:46 +0800

    Add files via upload

---
+++ build2/precache-manifest.9869dc04e68b8ff7956042c3773d1921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4faf8c9cfbb7ac0c6e6f8b1d00025ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3877d184264e39ab40b8",
+    "url": "/static/css/main.b5cd55e8.chunk.css"
+  },
+  {
+    "revision": "ab6aa21d813eadebc74a",
+    "url": "/static/js/2.e720ae4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e720ae4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3877d184264e39ab40b8",
+    "url": "/static/js/main.719998b1.chunk.js"
+  },
+  {
+    "revision": "f86d1615552f35c38c25",
+    "url": "/static/js/runtime-main.1f2819f4.js"
+  }
+]);
